a78b7b61553d5e5c09e954beaffad720c767b98f
Maksim <max9599@gmail.com>
2014-10-29 21:04:54 +0200

    Väike muudatus

--- project.py
+++ project.py
@@ -1,13 +1,20 @@
 #Lets do it!
 
+
 def func1():
     print("Maksim tegi oma töö ära")
+
 
 def func2():
     print("Mina ka (Joonas)")
 
+
 def main():
-	print("Programm alustab tööd")
-	func1()
-	func2()
-	print("Programmi töö lõpp")
+    print("Programm alustab tööd")
+    func1()
+    func2()
+    print("Programmi töö lõpp")
+
+
+if __name__ == "__main__":
+    main()
