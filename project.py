14171a41568ebd76a169ae4605987389fa6b8eda
EestiParim <olen2ge@gmail.com>
2014-10-29 21:09:06 +0200

    lol

--- project.py
+++ project.py
@@ -1,12 +1,19 @@
 #Lets do it!
 
 
+
 def func1():
     print("Maksim tegi oma töö ära")
 
 
 def func2():
     print("Mina ka (Joonas)")
+
+def func3():
+    arv1=4
+    arv2=5
+    vanus=((arv1*arv2)-1)
+    Print ("Tõnise vanus on:",vanus)
 
 
 def main():
